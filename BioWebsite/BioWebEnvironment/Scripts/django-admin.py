69f1e2496988df954d6d5ea4d1a5622f578f9046
Daniel Andrejczyk <andrejczykd@gmail.com>
2020-07-31 22:02:28 -0500

    Add project files.

---
+++ BioWebsite/BioWebEnvironment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Andrejczyk\source\repos\BioWebsite\BioWebsite\BioWebEnvironment\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
