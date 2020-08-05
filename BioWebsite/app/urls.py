0ce79dd6f33418bf700e3c1074a449367d38e48e
Daniel Andrejczyk <andrejczykd@gmail.com>
2020-08-05 15:02:37 -0500

    Working template now lists articles as links to open the full article on another url

--- BioWebsite/app/urls.py
+++ BioWebsite/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:id>/', views.content_view, name='content_view'),
 ]
--- BioWebsite/app/views.py
+++ BioWebsite/app/views.py
@@ -1,9 +1,17 @@
 import io
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from wsgiref.util import FileWrapper
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.files import File
+from django.template import loader
+from .models import Article
 
 # Create your views here.
 def home(request):
-    return render(request, "app/home.html")
+    articles = Article.objects.order_by('-publish_date')[:1]
+    context = {'articles': articles}
+    return render(request, 'app/ArticleList.html', context)
+
+def content_view(request, id):
+    article = get_object_or_404(Article, pk=id)
+    return render(request, 'app/ArticleView.html', {'article': article})
